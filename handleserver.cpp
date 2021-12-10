e7b38e013526a07ea67a3e5b1f71c1faeb3facfc
OceanPresent <oceanpresent@163.com>
2021-12-11 01:32:29 +0800

    好好地改了一顿！

---
+++ handleserver.cpp
@@ -0,0 +1,6 @@
+#include "handleserver.h"
+
+HandleServer::HandleServer()
+{
+
+}
---
+++ handleserver.h
@@ -0,0 +1,12 @@
+#ifndef HANDLESERVER_H
+#define HANDLESERVER_H
+
+#include <QObject>
+
+class HandleServer
+{
+public:
+    HandleServer();
+};
+
+#endif // HANDLESERVER_H
